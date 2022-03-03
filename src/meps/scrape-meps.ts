9f9497901dcbdca50441b1972c01e4c1c3a96aef
David Ruisinger <david.ruisinger@gmail.com>
2022-03-03 12:20:17 +0100

    chore: remove debug limitation

--- src/meps/scrape-meps.ts
+++ src/meps/scrape-meps.ts
@@ -38,54 +38,52 @@ export async function scrapeMEPs(): Promise<EnrichedMEP[]> {
   // Fetch details for each MEP
   const enrichedMEPs: EnrichedMEP[] = []
   for (const [i, mep] of allMEPs.entries()) {
-    if (i === 7) {
-      const mepDetailsHTML = (
-        await axios.get(`${MEP_DETAIL_PAGE_BASE_URL}/${mep.id}`)
-      ).data
+    const mepDetailsHTML = (
+      await axios.get(`${MEP_DETAIL_PAGE_BASE_URL}/${mep.id}`)
+    ).data
 
-      const $MEP = cheerio.load(mepDetailsHTML)
+    const $MEP = cheerio.load(mepDetailsHTML)
 
-      const email = parseEmailHref($MEP('.link_email').attr('href') || '')
-      if (!email) {
-        console.error(
-          `\nCould not scrape email for ${mep.fullName} (ID: ${mep.id})`
-        )
-      }
-
-      const imageUrl = `${MEP_PHOTO_BASE_URL}/${
-        $MEP('.erpl_image-frame > span > img').attr('src') || ''
-      }`
-      if (!imageUrl) {
-        console.error(
-          `\nCould not scrape imageUrl for ${mep.fullName} (ID: ${mep.id})`
-        )
-      }
-
-      const badges: string[] = []
-      $MEP('.badges').each((i, el) => {
-        const badgeId = $MEP(el).find('.erpl_badge').text()
-        badges.push(badgeId)
-      })
-
-      const countryCode = countries[mep.country]
-      if (!countryCode) {
-        console.error(`\nCould not find countryCode for ${mep.country}`)
-      }
-
-      enrichedMEPs.push({
-        badges,
-        country: mep.country || '',
-        countryCode,
-        email,
-        fullName: mep.fullName || '',
-        id: mep.id || '',
-        imageUrl,
-        nationalPoliticalGroup: mep.nationalPoliticalGroup || '',
-        politicalGroup: mep.politicalGroup || '',
-      })
-
-      process.stdout.write(`\r${i + 1} of ${allMEPs.length} MEPs scraped`)
+    const email = parseEmailHref($MEP('.link_email').attr('href') || '')
+    if (!email) {
+      console.error(
+        `\nCould not scrape email for ${mep.fullName} (ID: ${mep.id})`
+      )
     }
+
+    const imageUrl = `${MEP_PHOTO_BASE_URL}/${
+      $MEP('.erpl_image-frame > span > img').attr('src') || ''
+    }`
+    if (!imageUrl) {
+      console.error(
+        `\nCould not scrape imageUrl for ${mep.fullName} (ID: ${mep.id})`
+      )
+    }
+
+    const badges: string[] = []
+    $MEP('.badges').each((i, el) => {
+      const badgeId = $MEP(el).find('.erpl_badge').text()
+      badges.push(badgeId)
+    })
+
+    const countryCode = countries[mep.country]
+    if (!countryCode) {
+      console.error(`\nCould not find countryCode for ${mep.country}`)
+    }
+
+    enrichedMEPs.push({
+      badges,
+      country: mep.country || '',
+      countryCode,
+      email,
+      fullName: mep.fullName || '',
+      id: mep.id || '',
+      imageUrl,
+      nationalPoliticalGroup: mep.nationalPoliticalGroup || '',
+      politicalGroup: mep.politicalGroup || '',
+    })
+
+    process.stdout.write(`\r${i + 1} of ${allMEPs.length} MEPs scraped`)
   }
 
   console.info('\nScraping MEPs ✔')
