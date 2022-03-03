44df5bf3091c1b90a260a47a00c36049bf1fa95a
David Ruisinger <david.ruisinger@gmail.com>
2022-03-03 14:58:16 +0100

    chore: update file name

--- src/meps/config.ts
+++ src/meps/config.ts
@@ -1,7 +1,7 @@
 export const ALL_MEPS_XML_SRC =
   'https://www.europarl.europa.eu/meps/en/full-list/xml'
 export const BADGE_LOCALIZATIONS = ['DE', 'EN', 'ES', 'FR']
-export const BADGES_FILE_NAME = 'badges-by-id.json'
+export const BADGES_FILE_NAME = 'mep-badges-by-id.json'
 export const COMMITTEE_DETAIL_PAGE_URL =
   'https://www.europarl.europa.eu/committees/{{locale}}/about/list-of-committees'
 export const DELEGATION_DETAIL_PAGE_URL =
